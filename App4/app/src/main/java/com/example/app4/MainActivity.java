878d0d2f6acc716958e78e3aaf53f7a9a2398ad6
lallebj <lallebj@users.noreply.github.com>
2019-03-13 17:34:53 +0100

    commit

--- App4/app/src/main/java/com/example/app4/MainActivity.java
+++ App4/app/src/main/java/com/example/app4/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.app4;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int[] sequence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void generateSequence(){
+        for (int i = 0; i < 1000; i++){
+            sequence[i] = (int) Math.random()*100;
+        }
+    }
 }
+
